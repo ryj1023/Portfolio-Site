4f4dbf77301867572fa4e9ecc154c4cf9df30b26
Ryan Johnson <ryj1023@yahoo.com>
2019-06-08 11:41:12 -0400

    nav bar collapse and jump menu positioning

--- build/portfolio-site.1.0.0.js
+++ build/portfolio-site.1.0.0.js
@@ -45,6 +45,81 @@
 /***/ (function(module, exports) {
 
 	"use strict";
+	
+	(function (document, history, location) {
+	  var HISTORY_SUPPORT = !!(history && history.pushState);
+	
+	  var anchorScrolls = {
+	    ANCHOR_REGEX: /^#[^ ]+$/,
+	    OFFSET_HEIGHT_PX: 46,
+	
+	    /**
+	     * Establish events, and fix initial scroll position if a hash is provided.
+	     */
+	    init: function init() {
+	      this.scrollToCurrent();
+	      window.addEventListener("hashchange", this.scrollToCurrent.bind(this));
+	      document.body.addEventListener("click", this.delegateAnchors.bind(this));
+	    },
+	
+	    /**
+	     * Return the offset amount to deduct from the normal scroll position.
+	     * Modify as appropriate to allow for dynamic calculations
+	     */
+	    getFixedOffset: function getFixedOffset() {
+	      return this.OFFSET_HEIGHT_PX;
+	    },
+	
+	    /**
+	     * If the provided href is an anchor which resolves to an element on the
+	     * page, scroll to it.
+	     * @param  {String} href
+	     * @return {Boolean} - Was the href an anchor.
+	     */
+	    scrollIfAnchor: function scrollIfAnchor(href, pushToHistory) {
+	      var match, rect, anchorOffset;
+	
+	      if (!this.ANCHOR_REGEX.test(href)) {
+	        return false;
+	      }
+	
+	      match = document.getElementById(href.slice(1));
+	
+	      if (match) {
+	        rect = match.getBoundingClientRect();
+	        anchorOffset = window.pageYOffset + rect.top - this.getFixedOffset();
+	        window.scrollTo(window.pageXOffset, anchorOffset);
+	
+	        // Add the state to history as-per normal anchor links
+	        if (HISTORY_SUPPORT && pushToHistory) {
+	          history.pushState({}, document.title, location.pathname + href);
+	        }
+	      }
+	
+	      return !!match;
+	    },
+	
+	    /**
+	     * Attempt to scroll to the current location's hash.
+	     */
+	    scrollToCurrent: function scrollToCurrent() {
+	      this.scrollIfAnchor(window.location.hash);
+	    },
+	
+	    /**
+	     * If the click event's target was an anchor, fix the scroll position.
+	     */
+	    delegateAnchors: function delegateAnchors(e) {
+	      var elem = e.target;
+	
+	      if (elem.nodeName === "A" && this.scrollIfAnchor(elem.getAttribute("href"), true)) {
+	        e.preventDefault();
+	      }
+	    }
+	  };
+	
+	  window.addEventListener("DOMContentLoaded", anchorScrolls.init.bind(anchorScrolls));
+	})(window.document, window.history, window.location);
 
 /***/ })
 /******/ ]);
